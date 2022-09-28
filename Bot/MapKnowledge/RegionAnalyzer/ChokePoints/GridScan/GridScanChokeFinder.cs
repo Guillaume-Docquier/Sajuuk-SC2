fcff4794db3b5114f28aba3bb0d98a10e56c3a59
Guillaume Docquier <guillaumedocquier@hotmail.com>
2022-09-28 17:27:12 -0400

    Some cleaning up before continuing

--- Bot/MapKnowledge/RegionAnalyzer/ChokePoints/GridScan/GridScanChokeFinder.cs
+++ Bot/MapKnowledge/RegionAnalyzer/ChokePoints/GridScan/GridScanChokeFinder.cs
@@ -8,7 +8,7 @@ using Bot.Wrapper;
 namespace Bot.MapKnowledge;
 
 // TODO GD Considering the obstacles (resources, rocks) might be interesting at some point
-public static class GridScanChokeFinder {
+public static partial class GridScanChokeFinder {
     private const int StartingAngle = 0;
     private const int MaxAngle = 175;
     private const int AngleIncrement = 5;
@@ -16,101 +16,14 @@ public static class GridScanChokeFinder {
     private static int MaxX => Controller.GameInfo.StartRaw.MapSize.X;
     private static int MaxY => Controller.GameInfo.StartRaw.MapSize.Y;
 
-    private class Line {
-        public List<Vector3> OrderedTraversedCells { get; }
-        public int Angle { get; }
-
-        public Vector3 Start { get; }
-        public Vector3 End { get; }
-        public float Length { get; }
-
-        public Line(Vector3 start, Vector3 end, int angle) {
-            var centerOfStart = start.AsWorldGridCenter();
-
-            OrderedTraversedCells = start.GetPointsInBetween(end)
-                .OrderBy(current => current.HorizontalDistanceTo(centerOfStart))
-                .ToList();
-
-            Start = OrderedTraversedCells[0];
-            End = OrderedTraversedCells.Last();
-            Length = Start.HorizontalDistanceTo(End);
-
-            Angle = angle;
-        }
-
-        public Line(List<Vector3> orderedTraversedCells, int angle) {
-            OrderedTraversedCells = orderedTraversedCells;
-
-            Start = OrderedTraversedCells[0];
-            End = OrderedTraversedCells.Last();
-            Length = Start.HorizontalDistanceTo(End);
-
-            Angle = angle;
-        }
-    }
-
-    private class Node : IHavePosition {
-        private const float MaxVisionDistance = 15f;
-
-        public Vector3 Position { get; }
-        public List<Line> Lines { get; } = new List<Line>();
-
-        public Line ShortestLine { get; private set; }
-        public Line BestChokeLine { get; private set; }
-        public float ChokeScore { get; private set; }
-
-        public Node(Vector3 position) {
-            Position = position;
-        }
-
-        public void UpdateShortestLine() {
-            ShortestLine = Lines.MinBy(line => line.Length);
-        }
-
-        public void UpdateBestChokeLine() {
-            Line bestChokeLine = null;
-            float bestChokeScore = 0f;
-
-            foreach (var line in Lines) {
-                var perpendicularLineAngle = (line.Angle + 90) % (MaxAngle + AngleIncrement);
-                var perpendicularLine = Lines.Find(otherLine => otherLine.Angle == perpendicularLineAngle)!;
-
-                var startDistance = Math.Min(MaxVisionDistance, perpendicularLine.Start.HorizontalDistanceTo(Position));
-                var endDistance = Math.Min(MaxVisionDistance, perpendicularLine.End.HorizontalDistanceTo(Position));
-
-                var startChokeScore = (startDistance + 1) / (line.Length + 1);
-                var endChokeScore = (endDistance + 1) / (line.Length + 1);
-
-                var chokeScore = Math.Min(startChokeScore, endChokeScore);
-                if (chokeScore > bestChokeScore) {
-                    bestChokeLine = line;
-                    bestChokeScore = chokeScore;
-                }
-            }
-
-            BestChokeLine = bestChokeLine;
-            ChokeScore = bestChokeScore;
-        }
-    }
-
     public static List<ChokePoint> FindChokePoints() {
         var nodes = ComputeWalkableNodesInMap();
         Logger.Info("Computed {0} nodes", nodes.Count);
 
-        for (var angle = StartingAngle; angle <= MaxAngle; angle += AngleIncrement) {
-            var lines = CreateLines(angle);
-            lines = BreakDown(lines);
+        var lines = CreateVisionLines(nodes.Count);
+        Logger.Info("Computed {0} total vision lines", lines.Count);
 
-            foreach (var line in lines) {
-                // TODO GD Store cell index with the line for faster lookup later on?
-                for (var cellIndex = 0; cellIndex < line.OrderedTraversedCells.Count; cellIndex++) {
-                    nodes[line.OrderedTraversedCells[cellIndex]].Lines.Add(line);
-                }
-            }
-
-            var nbNodesCovered = lines.SelectMany(line => line.OrderedTraversedCells).ToHashSet().Count;
-            Logger.Info("Created {0,4} lines at {1,3} degrees covering {2,5} nodes ({3,3:P0})", lines.Count, angle, nbNodesCovered, (float)nbNodesCovered / nodes.Count);
-        }
+        MarkTraversedNodes(nodes, lines);
 
         // Determine chokes
         foreach (var node in nodes.Values) {
@@ -118,7 +31,7 @@ public static class GridScanChokeFinder {
         }
 
         var uniqueShortestLines = nodes.Values
-            .Select(node => node.ShortestLine)
+            .Select(node => node.ShortestVisionLine)
             .ToHashSet()
             .ToList();
 
@@ -130,7 +43,7 @@ public static class GridScanChokeFinder {
 
         var uniqueBestChokeLines = nodes.Values
             .Where(node => node.ChokeScore >= 1.4f)
-            .Select(node => node.BestChokeLine)
+            .Select(node => node.BestChokeVisionLine)
             .GroupBy(line => line)
             .Where(group => group.Count() > 2)
             .Select(group => group.Key)
@@ -160,19 +73,34 @@ public static class GridScanChokeFinder {
         return nodes;
     }
 
-    private static List<Line> CreateLines(int angleInDegrees) {
+    private static List<VisionLine> CreateVisionLines(int nodeCount) {
+        var allLines = new List<VisionLine>();
+        for (var angle = StartingAngle; angle <= MaxAngle; angle += AngleIncrement) {
+            var lines = CreateLinesAtAnAngle(angle);
+            lines = BreakDownIntoContinuousSegments(lines);
+
+            var nbNodesCovered = lines.SelectMany(line => line.OrderedTraversedCells).ToHashSet().Count;
+            Logger.Info("Created {0,4} lines at {1,3} degrees covering {2,5} nodes ({3,3:P0})", lines.Count, angle, nbNodesCovered, (float)nbNodesCovered / nodeCount);
+
+            allLines.AddRange(lines);
+        }
+
+        return allLines;
+    }
+
+    private static List<VisionLine> CreateLinesAtAnAngle(int angleInDegrees) {
         var lineLength = (int)Math.Ceiling(Math.Sqrt(MaxX * MaxX + MaxY * MaxY));
         var origin = new Vector3 { X = MaxX / 2f, Y = MaxY / 2f };
         var paddingX = (int)(lineLength / 2f - origin.X);
         var paddingY = (int)(lineLength / 2f - origin.Y);
         var angleInRadians = DegToRad(angleInDegrees);
 
-        var lines = new List<Line>();
+        var lines = new List<VisionLine>();
         for (var y = -paddingY; y < MaxY + paddingY; y++) {
             var start = new Vector3(-paddingX, y, 0).Rotate2D(angleInRadians, origin);
             var end = new Vector3(MaxX + paddingX, y, 0).Rotate2D(angleInRadians, origin);
 
-            lines.Add(new Line(start, end, angleInDegrees));
+            lines.Add(new VisionLine(start, end, angleInDegrees));
         }
 
         return lines;
@@ -182,28 +110,28 @@ public static class GridScanChokeFinder {
         return Math.PI / 180 * degrees;
     }
 
-    private static List<Line> BreakDown(List<Line> lines) {
-        return lines.SelectMany(BreakDown).ToList();
+    private static List<VisionLine> BreakDownIntoContinuousSegments(IEnumerable<VisionLine> lines) {
+        return lines.SelectMany(BreakDownIntoContinuousSegments).ToList();
     }
 
-    private static List<Line> BreakDown(Line line) {
-        var lines = new List<Line>();
+    private static List<VisionLine> BreakDownIntoContinuousSegments(VisionLine visionLine) {
+        var lines = new List<VisionLine>();
 
         var cellIndex = 0;
-        while (cellIndex < line.OrderedTraversedCells.Count) {
-            var startCellIndex = GoToStartOfNextLine(line, cellIndex);
+        while (cellIndex < visionLine.OrderedTraversedCells.Count) {
+            var startCellIndex = GoToStartOfNextLine(visionLine, cellIndex);
             if (startCellIndex < 0) {
                 break;
             }
 
-            var endCellIndex = GoToEndOfLine(line, startCellIndex);
+            var endCellIndex = GoToEndOfLine(visionLine, startCellIndex);
 
-            var cells = line.OrderedTraversedCells
+            var cells = visionLine.OrderedTraversedCells
                 .Skip(startCellIndex)
                 .Take(endCellIndex - startCellIndex + 1)
                 .ToList();
 
-            lines.Add(new Line(cells, line.Angle));
+            lines.Add(new VisionLine(cells, visionLine.Angle));
 
             cellIndex = endCellIndex + 1;
         }
@@ -215,13 +143,13 @@ public static class GridScanChokeFinder {
     /// Iterate over the line's cells until we meet a walkable cell.
     /// Returns the index of that cell, or -1 if not found.
     /// </summary>
-    /// <param name="line">The line to iterate over</param>
+    /// <param name="visionLine">The line to iterate over</param>
     /// <param name="startCellIndex">The cell index to start at</param>
     /// <returns>The index of the next walkable cell or -1 if none</returns>
-    private static int GoToStartOfNextLine(Line line, int startCellIndex) {
+    private static int GoToStartOfNextLine(VisionLine visionLine, int startCellIndex) {
         var currentCellIndex = startCellIndex;
-        while (currentCellIndex < line.OrderedTraversedCells.Count) {
-            if (MapAnalyzer.IsWalkable(line.OrderedTraversedCells[currentCellIndex], includeObstacles: false)) {
+        while (currentCellIndex < visionLine.OrderedTraversedCells.Count) {
+            if (MapAnalyzer.IsWalkable(visionLine.OrderedTraversedCells[currentCellIndex], includeObstacles: false)) {
                 return currentCellIndex;
             }
 
@@ -235,18 +163,18 @@ public static class GridScanChokeFinder {
     /// Iterate over the line's cells until we meet an unwalkable cell.
     /// Returns the index of the last walkable cell encountered or -1 if none.
     /// </summary>
-    /// <param name="line">The line to iterate over</param>
+    /// <param name="visionLine">The line to iterate over</param>
     /// <param name="startCellIndex">The cell index to start at</param>
     /// <returns>The index of the last walkable cell or -1 if none</returns>
-    private static int GoToEndOfLine(Line line, int startCellIndex) {
+    private static int GoToEndOfLine(VisionLine visionLine, int startCellIndex) {
         if (startCellIndex < 0) {
             return startCellIndex;
         }
 
         var currentCellIndex = startCellIndex;
 
-        while (currentCellIndex < line.OrderedTraversedCells.Count) {
-            if (!MapAnalyzer.IsWalkable(line.OrderedTraversedCells[currentCellIndex], includeObstacles: false)) {
+        while (currentCellIndex < visionLine.OrderedTraversedCells.Count) {
+            if (!MapAnalyzer.IsWalkable(visionLine.OrderedTraversedCells[currentCellIndex], includeObstacles: false)) {
                 return currentCellIndex - 1;
             }
 
@@ -256,13 +184,21 @@ public static class GridScanChokeFinder {
         return currentCellIndex - 1;
     }
 
+    private static void MarkTraversedNodes(IReadOnlyDictionary<Vector3, Node> nodes, List<VisionLine> lines) {
+        foreach (var line in lines) {
+            foreach (var cell in line.OrderedTraversedCells) {
+                nodes[cell].VisionLines.Add(line);
+            }
+        }
+    }
+
     private static void DebugLineLengths(List<Node> nodes) {
-        var minLength = nodes.Min(node => node.ShortestLine.Length);
-        var maxLength = nodes.Max(node => node.ShortestLine.Length);
+        var minLength = nodes.Min(node => node.ShortestVisionLine.Length);
+        var maxLength = nodes.Max(node => node.ShortestVisionLine.Length);
 
         foreach (var node in nodes) {
-            var textColor = Colors.Gradient(Colors.DarkRed, Colors.White, LinScale(node.ShortestLine.Length, minLength, maxLength));
-            Program.GraphicalDebugger.AddText($"{node.ShortestLine.Length:F0}", worldPos: node.Position.WithWorldHeight().ToPoint(), color: textColor, size: 13);
+            var textColor = Colors.Gradient(Colors.DarkRed, Colors.White, LinScale(node.ShortestVisionLine.Length, minLength, maxLength));
+            Program.GraphicalDebugger.AddText($"{node.ShortestVisionLine.Length:F0}", worldPos: node.Position.WithWorldHeight().ToPoint(), color: textColor, size: 13);
         }
     }
 
@@ -288,7 +224,7 @@ public static class GridScanChokeFinder {
         return (number - min) / (max - min);
     }
 
-    private static void DebugLines(List<Line> lines) {
+    private static void DebugLines(List<VisionLine> lines) {
         foreach (var line in lines) {
             Program.GraphicalDebugger.AddLink(line.Start.WithWorldHeight(), line.End.WithWorldHeight(), Colors.Orange, withText: false);
         }
---
+++ Bot/MapKnowledge/RegionAnalyzer/ChokePoints/GridScan/VisionLine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Bot.ExtensionMethods;
+
+namespace Bot.MapKnowledge;
+
+public static partial class GridScanChokeFinder {
+    private class VisionLine {
+        public List<Vector3> OrderedTraversedCells { get; }
+        public int Angle { get; }
+
+        public Vector3 Start { get; }
+        public Vector3 End { get; }
+        public float Length { get; }
+
+        public VisionLine(Vector3 start, Vector3 end, int angle) {
+            var centerOfStart = start.AsWorldGridCenter();
+
+            OrderedTraversedCells = start.GetPointsInBetween(end)
+                .OrderBy(current => current.HorizontalDistanceTo(centerOfStart))
+                .ToList();
+
+            Start = OrderedTraversedCells[0];
+            End = OrderedTraversedCells.Last();
+            Length = Start.HorizontalDistanceTo(End);
+
+            Angle = angle;
+        }
+
+        public VisionLine(List<Vector3> orderedTraversedCells, int angle) {
+            OrderedTraversedCells = orderedTraversedCells;
+
+            Start = OrderedTraversedCells[0];
+            End = OrderedTraversedCells.Last();
+            Length = Start.HorizontalDistanceTo(End);
+
+            Angle = angle;
+        }
+    }
+}
