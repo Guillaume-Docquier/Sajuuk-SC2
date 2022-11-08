6f9bfaefb4462b2c598bc1ec34cf0ba1ff2841e3
Guillaume Docquier <guillaumedocquier@hotmail.com>
2022-11-07 23:53:29 -0500

    Spin 180 degrees and have different colors for perpendicular lines

--- Bot/VideoClips/Clips/PerpendicularLinesScanClip.cs
+++ Bot/VideoClips/Clips/PerpendicularLinesScanClip.cs
@@ -6,6 +6,7 @@ using Bot.ExtensionMethods;
 using Bot.MapKnowledge;
 using Bot.Utils;
 using Bot.VideoClips.Manim.Animations;
+using SC2APIProtocol;
 
 namespace Bot.VideoClips.Clips;
 
@@ -15,7 +16,11 @@ public class PerpendicularLinesScanClip : Clip {
             .WithDurationInSeconds(1);
         AddAnimation(centerCameraAnimation);
 
-        var pauseAnimation = new PauseAnimation(centerCameraAnimation.AnimationEndFrame)
+        var locationAnimation = new CellDrawingAnimation(sceneLocation.ToVector3(), centerCameraAnimation.AnimationEndFrame)
+            .WithDurationInSeconds(1);
+        AddAnimation(locationAnimation);
+
+        var pauseAnimation = new PauseAnimation(locationAnimation.AnimationEndFrame)
             .WithDurationInSeconds(2);
         AddAnimation(pauseAnimation);
 
@@ -24,31 +29,31 @@ public class PerpendicularLinesScanClip : Clip {
 
     private void ScanTerrainWithLines(Vector2 location, int startFrame) {
         var previousAnimationEndFrame = startFrame;
-        for (var angle = 0; angle <= 90; angle += 1) {
+        for (var angle = 0; angle <= 180; angle += 1) {
             previousAnimationEndFrame = DrawCross(location, angle, previousAnimationEndFrame);
         }
     }
 
     private int DrawCross(Vector2 origin, double angle, int startFrame) {
-        DrawRaySegments(RayCasting.RayCast(origin, MathUtils.DegToRad(angle), cell => !MapAnalyzer.IsWalkable(cell)).ToList(), startFrame);
-        DrawRaySegments(RayCasting.RayCast(origin, MathUtils.DegToRad(angle + 90), cell => !MapAnalyzer.IsWalkable(cell)).ToList(), startFrame);
-        DrawRaySegments(RayCasting.RayCast(origin, MathUtils.DegToRad(angle + 180), cell => !MapAnalyzer.IsWalkable(cell)).ToList(), startFrame);
+        DrawRaySegments(RayCasting.RayCast(origin, MathUtils.DegToRad(angle), cell => !MapAnalyzer.IsWalkable(cell)).ToList(), startFrame, Colors.BrightGreen);
+        DrawRaySegments(RayCasting.RayCast(origin, MathUtils.DegToRad(angle + 90), cell => !MapAnalyzer.IsWalkable(cell)).ToList(), startFrame, Colors.Red);
+        DrawRaySegments(RayCasting.RayCast(origin, MathUtils.DegToRad(angle + 180), cell => !MapAnalyzer.IsWalkable(cell)).ToList(), startFrame, Colors.BrightGreen);
 
-        return DrawRaySegments(RayCasting.RayCast(origin, MathUtils.DegToRad(angle + 270), cell => !MapAnalyzer.IsWalkable(cell)).ToList(), startFrame);
+        return DrawRaySegments(RayCasting.RayCast(origin, MathUtils.DegToRad(angle + 270), cell => !MapAnalyzer.IsWalkable(cell)).ToList(), startFrame, Colors.Red);
     }
 
-    private int DrawRaySegments(List<RayCasting.RayCastResult> rayCastResults, int startFrame) {
+    private int DrawRaySegments(List<RayCasting.RayCastResult> rayCastResults, int startFrame, Color color) {
         var previousRayEnd = rayCastResults[0].RayIntersection;
         foreach (var rayCastResult in rayCastResults) {
-            DrawRay(previousRayEnd, rayCastResult.RayIntersection, startFrame);
+            DrawRay(previousRayEnd, rayCastResult.RayIntersection, startFrame, color);
             previousRayEnd = rayCastResult.RayIntersection;
         }
 
         return startFrame + 1; // Yeah I'm lazy at this point
     }
 
-    private int DrawRay(Vector2 origin, Vector2 destination, int startFrame) {
-        var lineDrawingAnimation = new LineDrawingAnimation(origin.ToVector3(), destination.ToVector3(), Colors.DarkRed, startFrame)
+    private int DrawRay(Vector2 origin, Vector2 destination, int startFrame, Color color) {
+        var lineDrawingAnimation = new LineDrawingAnimation(origin.ToVector3(), destination.ToVector3(), color, startFrame)
             .WithPostAnimationDurationInFrames(1);
 
         AddAnimation(lineDrawingAnimation);
