ac26f648bc6441e8cec3e45321e09248369f1e18
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-03-26 19:31:20 -0400

    Only consider 1 mineral patch from outside the army region when trying to mineral walk, because all minerals will likely create the same path through the exit. Added comments, cleaned up the code a little.

--- Bot/Managers/WarManagement/ArmySupervision/UnitsControl/DroneKitingUnitsControl.cs
+++ Bot/Managers/WarManagement/ArmySupervision/UnitsControl/DroneKitingUnitsControl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using Bot.ExtensionMethods;
 using Bot.GameData;
 using Bot.GameSense;
@@ -28,23 +29,9 @@ public class DroneKitingUnitsControl : IUnitsControl {
         }
 
         var regimentCenter = Clustering.GetCenter(dronesThatNeedToKite);
-        var regimentRegion = regimentCenter.GetRegion();
 
-        var regimentRegionExits = regimentRegion.GetReachableNeighbors();
-        var mineralFields = regimentRegionExits.SelectMany(regimentRegionExit => {
-            // TODO GD We can reduce the amount of mineral fields by returning only 1 if we need to go through an exit
-            // TODO GD We should also consider the location of such minerals fields to be the exit region, since we'll route through there
-            var expandLocation = GetExpandLocationGoingThroughExit(regimentRegion, regimentRegionExit);
-            if (expandLocation != null) {
-                return expandLocation.ResourceCluster.Where(resource => Units.MineralFields.Contains(resource.UnitType));
-            }
-
-            return Enumerable.Empty<Unit>();
-        })
-            .ToList();
-
-        // No mineral fields to walk to, we can't act.
-        if (mineralFields.Count <= 0) {
+        var candidateMineralFields = GetMineralFieldsToWalkTo(regimentCenter);
+        if (candidateMineralFields.Count <= 0) {
             return uncontrolledUnits;
         }
 
@@ -61,7 +48,7 @@ public class DroneKitingUnitsControl : IUnitsControl {
             }
 
             var enemiesToAvoid = potentialEnemiesToAvoid.Where(enemy => enemy.DistanceTo(regimentCenter) <= 5).ToList();
-            var mineralToWalkTo = GetMineralToWalkTo(drone, enemiesToAvoid, mineralFields);
+            var mineralToWalkTo = GetMineralFieldToWalkTo(drone, enemiesToAvoid, candidateMineralFields);
 
             if (mineralToWalkTo != null) {
                 drone.Gather(mineralToWalkTo);
@@ -105,13 +92,52 @@ public class DroneKitingUnitsControl : IUnitsControl {
     }
 
     /// <summary>
+    /// Find all mineral fields that can be used to mineral walk.
+    /// - Those in the current region
+    /// - One patch per exit from the current region
+    ///
+    /// We will return a list of tuples composed of the mineral field and a position representing the exit that the unit would go through when going to that mineral field.
+    /// In the case of mineral fields in the same region as the regimentPosition, their exit will be themselves because the unit won't be funneled through an exit.
+    /// </summary>
+    /// <param name="regimentPosition">Where the regiment currently is</param>
+    /// <returns>A list of tuples composed of the mineral field and a position representing the exit that the unit would go through when going to that mineral field.</returns>
+    private static List<(Unit unit, Vector2 exit)> GetMineralFieldsToWalkTo(Vector2 regimentPosition) {
+        var regimentRegion = regimentPosition.GetRegion();
+        var regimentRegionExits = regimentRegion.GetReachableNeighbors();
+
+        return regimentRegionExits.SelectMany(regimentRegionExit => {
+                var expandLocation = GetExpandLocationGoingThroughExit(regimentRegion, regimentRegionExit);
+                if (expandLocation == null) {
+                    return Enumerable.Empty<(Unit, Vector2)>();
+                }
+
+                // When the region is the regimentRegion, we return all minerals with their own position as exit.
+                // We keep all minerals because each of them will likely be in a different direction from the unit.
+                if (regimentRegionExit == regimentRegion) {
+                    return expandLocation.ResourceCluster
+                        .Where(resource => Units.MineralFields.Contains(resource.UnitType))
+                        .Select(resource => (resource, resource.Position.ToVector2()));
+                }
+
+                // When the region is not the regimentRegion, it means we'll have to go through an exit to get to the mineral field.
+                // Because of this, all minerals will more or less create the same path to that exit.
+                // The initial movement is going to be hard to accurately determine, so we'll use the center of the exit as an estimation.
+                var resource = expandLocation.ResourceCluster
+                    .First(resource => Units.MineralFields.Contains(resource.UnitType));
+
+                return new List<(Unit, Vector2)> { (resource, regimentRegionExit.Center) };
+            })
+            .ToList();
+    }
+
+    /// <summary>
     /// Gets the mineral from mineralFields that the given drone should to walk to in order to avoid enemiesToAvoid.
     /// </summary>
     /// <param name="drone">The drone that needs to run</param>
     /// <param name="enemiesToAvoid">The enemies that need to be avoided</param>
     /// <param name="mineralFields">The mineral fields we can walk to</param>
     /// <returns>The mineral field to walk to, or null if there are no good choices</returns>
-    private static Unit GetMineralToWalkTo(Unit drone, IReadOnlyCollection<Unit> enemiesToAvoid, IEnumerable<Unit> mineralFields) {
+    private static Unit GetMineralFieldToWalkTo(Unit drone, IReadOnlyCollection<Unit> enemiesToAvoid, IEnumerable<(Unit unit, Vector2 exit)> mineralFields) {
         if (enemiesToAvoid.Count <= 0) {
             return null;
         }
@@ -122,18 +148,18 @@ public class DroneKitingUnitsControl : IUnitsControl {
 
         // Find the mineral field with the angle closest to enemy-drone (angle 0 = directly fleeing the enemy
         var mineralToWalkTo = mineralFields.MinBy(mineralField => {
-            var mineralVector = drone.Position.DirectionTo(mineralField.Position);
+            var mineralVector = drone.Position.ToVector2().DirectionTo(mineralField.exit);
 
             return Math.Abs(enemyVector.GetRadAngleTo(mineralVector));
         });
 
-        var mineralVector = drone.Position.DirectionTo(mineralToWalkTo.Position);
+        var mineralVector = drone.Position.ToVector2().DirectionTo(mineralToWalkTo.exit);
         var mineralAngle = Math.Abs(enemyVector.GetRadAngleTo(mineralVector));
         if (mineralAngle > MathUtils.DegToRad(90)) {
             // If the best we got sends us towards the enemy, we don't do it
             return null;
         }
 
-        return mineralToWalkTo;
+        return mineralToWalkTo.unit;
     }
 }
