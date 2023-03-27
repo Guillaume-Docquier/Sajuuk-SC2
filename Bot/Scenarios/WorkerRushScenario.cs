c26883ede3a2b0ce15645418b4882214834465cf
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-03-27 00:33:27 -0400

    Fixed a bug where the army region mineral fields weren't being considered. Also fixed crashes due to clustering methods being able to return a position outside a region

--- Bot/Scenarios/WorkerRushScenario.cs
+++ Bot/Scenarios/WorkerRushScenario.cs
@@ -1,18 +1,16 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Bot.ExtensionMethods;
 using Bot.GameData;
-using Bot.GameSense;
 using Bot.MapKnowledge;
 using Bot.Utils;
 using Bot.Wrapper;
+using SC2APIProtocol;
 
 namespace Bot.Scenarios;
 
 // Simulates a worker rush by smallBly https://aiarena.net/bots/338/
 public class WorkerRushScenario: IScenario {
-    private const int DefaultTimingInSeconds = 1 * 60 + 50;
-    private const int SpawnDistance = 4; // Must be in aggro range otherwise they run away
+    private const int DefaultTimingInSeconds = 50;
 
     private readonly int _timingInSeconds;
     private bool _isScenarioDone = false;
@@ -27,15 +25,12 @@ public class WorkerRushScenario: IScenario {
         }
 
         if (Controller.Frame >= TimeUtils.SecsToFrames(_timingInSeconds)) {
-            var natural = Controller.GetUnits(UnitsTracker.OwnedUnits, Units.TownHalls)
-                .MinBy(townHall => Pathfinder.FindPath(townHall.Position.ToVector2(), MapAnalyzer.EnemyStartingLocation).Count);
+            var mainPosition = ExpandAnalyzer.GetExpand(Alliance.Self, ExpandType.Main).Position;
 
-            var pathFromNatural = Pathfinder.FindPath(natural!.Position.ToVector2(), MapAnalyzer.EnemyStartingLocation);
-
-            Logger.Debug("Spawning 12 zerglings {0} units away from natural", pathFromNatural[SpawnDistance].DistanceTo(natural));
+            Logger.Debug("Spawning 12 zerglings {0} units away from main", 0);
 
             // Spawned drones wouldn't be aggressive so we spawn zerglings instead
-            await Program.GameConnection.SendRequest(RequestBuilder.DebugCreateUnit(Owner.Enemy, Units.Zergling, 12, pathFromNatural[SpawnDistance].ToVector3()));
+            await Program.GameConnection.SendRequest(RequestBuilder.DebugCreateUnit(Owner.Enemy, Units.Zergling, 12, mainPosition.ToVector3()));
             Controller.SetRealTime("Worker rush started");
 
             _isScenarioDone = true;
