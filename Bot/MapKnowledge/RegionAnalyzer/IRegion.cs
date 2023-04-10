f9f149ed9ada43d38e3efe22b721c089e72d879d
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-04-10 00:43:20 -0400

    Fixed typos

--- Bot/MapKnowledge/RegionAnalyzer/IRegion.cs
+++ Bot/MapKnowledge/RegionAnalyzer/IRegion.cs
@@ -4,20 +4,19 @@ using SC2APIProtocol;
 
 namespace Bot.MapKnowledge;
 
-// TODO GD This interface is rather "fat", but changing it means a lot changes.
+// TODO GD This interface is rather "fat", but changing it represents a lot changes.
 public interface IRegion {
     public int Id { get; }
     public Color Color { get; }
 
-    Vector2 Center { get; }
+    public Vector2 Center { get; }
     public HashSet<Vector2> Cells { get; }
     public float ApproximatedRadius { get; }
 
-    RegionType Type { get; }
+    public RegionType Type { get; }
     public ExpandLocation ExpandLocation { get; }
     public HashSet<NeighboringRegion> Neighbors { get; }
-
-    bool IsObstructed { get; }
+    public bool IsObstructed { get; }
 
     public IEnumerable<IRegion> GetReachableNeighbors();
     public void UpdateObstruction();
