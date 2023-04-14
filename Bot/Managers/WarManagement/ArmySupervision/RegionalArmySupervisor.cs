9b90db3f777a03590931564f8737c270eda3f980
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-04-14 02:10:50 -0400

    Avoid enemies while getting into position

--- Bot/Managers/WarManagement/ArmySupervision/RegionalArmySupervisor.cs
+++ Bot/Managers/WarManagement/ArmySupervision/RegionalArmySupervisor.cs
@@ -31,21 +31,17 @@ public class RegionalArmySupervisor : Supervisor {
             return;
         }
 
-        var regionsWithFriendlyUnitPresence = SupervisedUnits
-            .Select(unit => unit.GetRegion())
-            .Where(region => region != null)
-            .ToHashSet();
-
         var approachRegions = _targetRegion.GetReachableNeighbors().ToHashSet();
+        // TODO GD Sometimes, units are in position outside of approach regions because they are avoiding the enemy. The should be included
         var unitsInPosition = GetUnitsInPosition(SupervisedUnits, _targetRegion, approachRegions);
-
         var enemyArmy = GetEnemyArmy(_targetRegion).ToList();
+
         if (unitsInPosition.GetForce() >= enemyArmy.GetForce()) {
-            Attack(unitsInPosition, _targetRegion);
-            MoveIntoPosition(SupervisedUnits.Except(unitsInPosition), approachRegions, regionsWithFriendlyUnitPresence);
+            Attack(unitsInPosition, _targetRegion, enemyArmy);
+            MoveIntoPosition(SupervisedUnits.Except(unitsInPosition).ToList(), approachRegions, enemyArmy);
         }
         else {
-            MoveIntoPosition(SupervisedUnits, approachRegions, regionsWithFriendlyUnitPresence);
+            MoveIntoPosition(SupervisedUnits, approachRegions, enemyArmy);
         }
     }
 
@@ -54,17 +50,12 @@ public class RegionalArmySupervisor : Supervisor {
     /// </summary>
     /// <param name="units">The units that must attack</param>
     /// <param name="targetRegion">The region to attack</param>
-    private void Attack(IReadOnlySet<Unit> units, IRegion targetRegion) {
+    /// <param name="enemyArmy">The enemy units to engage</param>
+    private void Attack(IReadOnlySet<Unit> units, IRegion targetRegion, IReadOnlyCollection<Unit> enemyArmy) {
         // TODO GD We can improve target selection
         var target = targetRegion.Center;
-        var targetUnits = UnitsTracker.EnemyUnits
-            .Concat(UnitsTracker.EnemyGhostUnits.Values)
-            .Where(unit => !unit.IsFlying) // TODO GD Bad hardcode, lazy me
-            .Where(unit => unit.GetRegion() == targetRegion)
-            .ToList();
-
-        if (targetUnits.Any()) {
-            target = targetUnits.MinBy(unit => unit.DistanceTo(targetRegion.Center)).Position.ToVector2();
+        if (enemyArmy.Any()) {
+            target = enemyArmy.MinBy(unit => unit.DistanceTo(targetRegion.Center)).Position.ToVector2();
         }
 
         var unhandledUnits = _unitsController.Execute(units);
@@ -75,16 +66,13 @@ public class RegionalArmySupervisor : Supervisor {
 
     /// <summary>
     /// Moves units into strike range by using the given approach regions.
+    /// Units will avoid engaging the enemy.
     /// </summary>
     /// <param name="units">The units to move</param>
     /// <param name="approachRegions">The regions in strike range of the global objective</param>
-    /// <param name="regionsWithFriendlyUnitPresence">Regions with friendly unit presence used to calculate regions to avoid</param>
-    private static void MoveIntoPosition(IEnumerable<Unit> units, IReadOnlyCollection<IRegion> approachRegions, IReadOnlyCollection<IRegion> regionsWithFriendlyUnitPresence) {
-        var reachableRegions = ComputeRegionsReach(regionsWithFriendlyUnitPresence);
-        var regionsOutOfReach = regionsWithFriendlyUnitPresence.ToDictionary(
-            region => region,
-            region => RegionAnalyzer.Regions.Except(reachableRegions[region]).ToHashSet()
-        );
+    /// <param name="enemyArmy">The enemy units to get in range of but avoid engaging</param>
+    private static void MoveIntoPosition(IReadOnlyCollection<Unit> units, IReadOnlyCollection<IRegion> approachRegions, IReadOnlyCollection<Unit> enemyArmy) {
+        var regionsOutOfReach = ComputeBlockedRegionsMap(units);
 
         var unitGroups = units
             .Where(unit => unit.GetRegion() != null)
@@ -99,18 +87,27 @@ public class RegionalArmySupervisor : Supervisor {
             }));
 
         foreach (var unitGroup in unitGroups) {
-            MoveTowards(unitGroup, unitGroup.Key, regionsOutOfReach);
+            MoveTowards(unitGroup, unitGroup.Key, regionsOutOfReach, enemyArmy);
         }
     }
 
     /// <summary>
     /// Moves towards the target region by following a path that avoids certain regions.
+    /// Units will avoid engaging the enemy.
     /// </summary>
     /// <param name="units">The units to move</param>
     /// <param name="targetRegion">The region to go to</param>
     /// <param name="blockedRegions">The regions to avoid going through</param>
-    private static void MoveTowards(IEnumerable<Unit> units, IRegion targetRegion, IDictionary<IRegion, HashSet<IRegion>> blockedRegions) {
+    /// <param name="enemyArmy">The enemy units to get in range of but avoid engaging</param>
+    private static void MoveTowards(IEnumerable<Unit> units, IRegion targetRegion, IDictionary<IRegion, HashSet<IRegion>> blockedRegions, IReadOnlyCollection<Unit> enemyArmy) {
         foreach (var unit in units) {
+            var closestEnemy = enemyArmy.MinBy(enemy => enemy.DistanceTo(unit) - enemy.MaxRange);
+            if (closestEnemy != null && closestEnemy.DistanceTo(unit) < closestEnemy.MaxRange * 2) {
+                // TODO GD We should avoid cornering ourselves, maybe we should go towards a region exit?
+                unit.MoveAwayFrom(closestEnemy.Position.ToVector2(), closestEnemy.MaxRange * 2);
+                continue;
+            }
+
             var unitRegion = unit.GetRegion();
 
             if (unitRegion == targetRegion) {
@@ -157,7 +154,11 @@ public class RegionalArmySupervisor : Supervisor {
     /// <param name="targetRegion">The target region.</param>
     /// <returns>The enemy units to defeat</returns>
     private static IEnumerable<Unit> GetEnemyArmy(IRegion targetRegion) {
-        var enemies = UnitsTracker.EnemyUnits.Concat(UnitsTracker.EnemyGhostUnits.Values).ToList();
+        var enemies = UnitsTracker.EnemyUnits
+            .Concat(UnitsTracker.EnemyGhostUnits.Values)
+            .Where(enemy => !enemy.IsFlying) // TODO GD Bad bad hardcode
+            .ToList();
+
         var clusteringResult = Clustering.DBSCAN(enemies, 2, 3);
 
         return clusteringResult.clusters
@@ -179,6 +180,25 @@ public class RegionalArmySupervisor : Supervisor {
     }
 
     /// <summary>
+    ///
+    /// </summary>
+    /// <param name="units"></param>
+    /// <returns></returns>
+    private static IDictionary<IRegion, HashSet<IRegion>> ComputeBlockedRegionsMap(IEnumerable<Unit> units) {
+        var regionsWithFriendlyUnitPresence = units
+            .Select(unit => unit.GetRegion())
+            .Where(region => region != null)
+            .ToHashSet();
+
+        var reachableRegions = ComputeRegionsReach(regionsWithFriendlyUnitPresence);
+
+        return regionsWithFriendlyUnitPresence.ToDictionary(
+            region => region,
+            region => RegionAnalyzer.Regions.Except(reachableRegions[region]).ToHashSet()
+        );
+    }
+
+    /// <summary>
     /// Computes the reach of each of the provided region.
     /// A region is reachable if there's a path to it that doesn't go through a dangerous region.
     /// </summary>
@@ -191,8 +211,8 @@ public class RegionalArmySupervisor : Supervisor {
             // TODO GD We can greatly optimize this by using dynamic programming
             reach[startingRegion] = TreeSearch.BreadthFirstSearch(
                 startingRegion,
-                region => region.GetReachableNeighbors(),
-                region => region != startingRegion && RegionTracker.GetForce(region, Alliance.Enemy) > 0
+                region => region.GetReachableNeighbors().Where(neighbor => RegionTracker.GetForce(neighbor, Alliance.Enemy) <= 0),
+                _ => false // TODO GD We might not need this predicate after all?
             ).ToList();
         }
 
