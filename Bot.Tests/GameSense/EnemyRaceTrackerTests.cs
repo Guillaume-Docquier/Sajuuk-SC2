6f4390b414ebdd75893b631fd0cc00d474b4ee96
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-04-22 14:40:34 -0400

    Test tagging behaviour of the EnemyRaceTracker. Also set sequential because we fiddle with global state

--- Bot.Tests/GameSense/EnemyRaceTrackerTests.cs
+++ Bot.Tests/GameSense/EnemyRaceTrackerTests.cs
@@ -1,11 +1,14 @@
 ﻿using Bot.GameData;
 using Bot.GameSense;
+using Bot.Tagging;
 using Bot.Tests.Fixtures;
 using Bot.Tests.Mocks;
 using SC2APIProtocol;
 
 namespace Bot.Tests.GameSense;
 
+// This is because we play with the global UnitsTracker.Instance
+[Collection("Sequential")]
 public class EnemyRaceTrackerTests : IClassFixture<KnowledgeBaseFixture>, IDisposable {
     public EnemyRaceTrackerTests() {
         UnitsTracker.Instance.Reset(); // TODO GD Review the test setup, right now the ResponseGameObservationUtils depend on the UnitsTracker.Instance
@@ -128,5 +131,125 @@ public class EnemyRaceTrackerTests : IClassFixture<KnowledgeBaseFixture>, IDispo
 
         // Assert
         Assert.Equal(expectedRace, enemyRaceTracker.EnemyRace);
+    }
+
+    [Theory]
+    [InlineData(Race.Terran, Race.Terran)]
+    [InlineData(Race.Protoss, Race.Protoss)]
+    [InlineData(Race.Zerg, Race.Zerg)]
+    [InlineData(Race.Random, Race.Random)]
+    public void GivenPlayerRaces_WhenUpdate_TagsEnemyRace(Race playerRace, Race enemyRace) {
+        // Arrange
+        var gameInfo = ResponseGameInfoUtils.CreateResponseGameInfo(playerRace: playerRace, enemyRace: enemyRace);
+        var observation = ResponseGameObservationUtils.CreateResponseObservation(units: BaseTestClass.GetInitialUnits());
+        var taggingService = new TestTaggingService();
+        var enemyRaceTracker = new EnemyRaceTracker(taggingService);
+
+        // Act
+        enemyRaceTracker.Update(observation, gameInfo);
+
+        // Assert
+        Assert.Single(taggingService.RacesTagged);
+        Assert.Equal(enemyRace, taggingService.RacesTagged[0]);
+    }
+
+    [Theory]
+    [InlineData(Race.Terran, Race.Terran)]
+    [InlineData(Race.Protoss, Race.Protoss)]
+    [InlineData(Race.Zerg, Race.Zerg)]
+    [InlineData(Race.Random, Race.Random)]
+    public void GivenPlayerRaces_WhenMultipleUpdates_TagsEnemyRaceOnlyOnce(Race playerRace, Race enemyRace) {
+        // Arrange
+        var gameInfo = ResponseGameInfoUtils.CreateResponseGameInfo(playerRace: playerRace, enemyRace: enemyRace);
+        var observation = ResponseGameObservationUtils.CreateResponseObservation(units: BaseTestClass.GetInitialUnits());
+        var taggingService = new TestTaggingService();
+        var enemyRaceTracker = new EnemyRaceTracker(taggingService);
+
+        // Act
+        enemyRaceTracker.Update(observation, gameInfo);
+        enemyRaceTracker.Update(observation, gameInfo);
+        enemyRaceTracker.Update(observation, gameInfo);
+        enemyRaceTracker.Update(observation, gameInfo);
+
+        // Assert
+        Assert.Single(taggingService.RacesTagged);
+        Assert.Equal(enemyRace, taggingService.RacesTagged[0]);
+    }
+
+    [Theory]
+    [MemberData(nameof(EnemyRandomRaceAndVisibleUnitsTestData))]
+    public void GivenEnemyRandomRaceAndVisibleUnits_WhenUpdate_ThenTagsRandomAndActualRace(IEnumerable<Unit> units, Race expectedRace) {
+        // Arrange
+        var gameInfo = ResponseGameInfoUtils.CreateResponseGameInfo(playerRace: Race.Zerg, enemyRace: Race.Random);
+        var observation = ResponseGameObservationUtils.CreateResponseObservation(units: units);
+
+        var taggingService = new TestTaggingService();
+        var enemyRaceTracker = new EnemyRaceTracker(taggingService);
+
+        // Act
+        enemyRaceTracker.Update(observation, gameInfo);
+        UnitsTracker.Instance.Update(observation, gameInfo);
+        enemyRaceTracker.Update(observation, gameInfo);
+
+        // Assert
+        Assert.Equal(new List<Race> { Race.Random, expectedRace }, taggingService.RacesTagged);
+    }
+
+    [Theory]
+    [MemberData(nameof(EnemyRandomRaceAndVisibleUnitsTestData))]
+    public void GivenEnemyRandomRaceAndVisibleUnits_WhenMultipleUpdates_ThenTagsRandomAndActualRaceOnce(IEnumerable<Unit> units, Race expectedRace) {
+        // Arrange
+        var gameInfo = ResponseGameInfoUtils.CreateResponseGameInfo(playerRace: Race.Zerg, enemyRace: Race.Random);
+        var observation = ResponseGameObservationUtils.CreateResponseObservation(units: units);
+
+        var taggingService = new TestTaggingService();
+        var enemyRaceTracker = new EnemyRaceTracker(taggingService);
+
+        // Act
+        enemyRaceTracker.Update(observation, gameInfo);
+        UnitsTracker.Instance.Update(observation, gameInfo);
+        enemyRaceTracker.Update(observation, gameInfo);
+        enemyRaceTracker.Update(observation, gameInfo);
+        enemyRaceTracker.Update(observation, gameInfo);
+        enemyRaceTracker.Update(observation, gameInfo);
+
+        // Assert
+        Assert.Equal(new List<Race> { Race.Random, expectedRace }, taggingService.RacesTagged);
+    }
+
+    private class TestTaggingService : ITaggingService {
+        public readonly List<Race> RacesTagged = new List<Race>();
+
+        public bool HasTagged(Tag tag) {
+            return false;
+        }
+
+        public void TagEarlyAttack() {
+            throw new NotImplementedException();
+        }
+
+        public void TagTerranFinisher() {
+            throw new NotImplementedException();
+        }
+
+        public void TagBuildDone(uint supply, float collectedMinerals, float collectedVespene) {
+            throw new NotImplementedException();
+        }
+
+        public void TagEnemyStrategy(string enemyStrategy) {
+            throw new NotImplementedException();
+        }
+
+        public void TagVersion(string version) {
+            throw new NotImplementedException();
+        }
+
+        public void TagMinerals(float collectedMinerals) {
+            throw new NotImplementedException();
+        }
+
+        public void TagEnemyRace(Race actualEnemyRace) {
+            RacesTagged.Add(actualEnemyRace);
+        }
     }
 }
