cc04578a0bbe7d92965e4c287083dec0e3633db7
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-04-30 21:10:47 -0400

    Increase json serialization reference depth because some map analysis has deep reference trees

--- Bot/MapAnalysis/RegionAnalysis/NeighboringRegion.cs
+++ Bot/MapAnalysis/RegionAnalysis/NeighboringRegion.cs
@@ -7,9 +7,12 @@ namespace Bot.MapAnalysis.RegionAnalysis;
 public class NeighboringRegion : INeighboringRegion {
     [JsonInclude] public Region ConcreteRegion;
     [JsonIgnore] public IRegion Region => ConcreteRegion;
+
     [JsonInclude] public HashSet<Vector2> Frontier { get; set; }
 
     [JsonConstructor]
+    public NeighboringRegion() {}
+
     public NeighboringRegion(Region concreteRegion, HashSet<Vector2> frontier) {
         ConcreteRegion = concreteRegion;
         Frontier = frontier;
