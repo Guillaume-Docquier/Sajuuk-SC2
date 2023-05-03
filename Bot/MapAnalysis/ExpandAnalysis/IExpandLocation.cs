e38df2ce525d5c323be7dbb957486222a6ebb606
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-05-03 00:25:31 -0400

    Removed useless property

--- Bot/MapAnalysis/ExpandAnalysis/IExpandLocation.cs
+++ Bot/MapAnalysis/ExpandAnalysis/IExpandLocation.cs
@@ -5,7 +5,6 @@ using Bot.MapAnalysis.RegionAnalysis;
 namespace Bot.MapAnalysis.ExpandAnalysis;
 
 public interface IExpandLocation {
-    public int Id { get; }
     public Vector2 Position { get; }
     public ExpandType ExpandType { get; }
     public HashSet<Unit> Resources { get; } // TODO GD We might not need to expose that
