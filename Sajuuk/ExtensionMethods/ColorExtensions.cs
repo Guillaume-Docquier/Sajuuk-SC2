c009a0cd014219f62acb08c32e9afdc51680ba3e
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-07-26 16:17:01 -0400

    Added images representing the choke scores

---
+++ Sajuuk/ExtensionMethods/ColorExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+
+namespace Sajuuk.ExtensionMethods;
+
+public static class ColorExtensions {
+    public static Color Gradient(this Color start, Color end, float percent) {
+        var deltaA = end.A - start.A;
+        var deltaR = end.R - start.R;
+        var deltaG = end.G - start.G;
+        var deltaB = end.B - start.B;
+
+        var debugA = Math.Round(start.A + deltaA * percent);
+        if ((int)debugA < 0) {
+            var stop = 1;
+        }
+
+        var gradient = Color.FromArgb(
+            (int)Math.Round(start.A + deltaA * percent),
+            (int)Math.Round(start.R + deltaR * percent),
+            (int)Math.Round(start.G + deltaG * percent),
+            (int)Math.Round(start.B + deltaB * percent)
+        );
+
+        return gradient;
+    }
+}
