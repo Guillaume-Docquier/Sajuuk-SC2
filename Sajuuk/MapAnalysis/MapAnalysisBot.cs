d336f9016105a0ada3cc22c4f4cd4048d1bb67bb
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-08-07 21:50:58 -0400

    Added MapAnalysisBotRunner. Removed DeprecatedBotRunner

--- Sajuuk/MapAnalysis/MapAnalysisBot.cs
+++ Sajuuk/MapAnalysis/MapAnalysisBot.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Sajuuk.Debugging.GraphicalDebugging;
 using Sajuuk.ExtensionMethods;
 using Sajuuk.GameSense;
+using Sajuuk.MapAnalysis.ExpandAnalysis;
 using Sajuuk.MapAnalysis.RegionAnalysis;
 using Sajuuk.Wrapper;
 using SC2APIProtocol;
@@ -13,7 +14,11 @@ public class MapAnalysisBot : IBot {
     private readonly IFrameClock _frameClock;
     private readonly ITerrainTracker _terrainTracker;
     private readonly IGraphicalDebugger _graphicalDebugger;
+    private readonly IExpandAnalyzer _expandAnalyzer;
     private readonly IRegionAnalyzer _regionAnalyzer;
+    private readonly ISc2Client _sc2Client;
+
+    private uint _quitAt = uint.MaxValue;
 
     public Race Race => Race.Zerg;
 
@@ -21,15 +26,19 @@ public class MapAnalysisBot : IBot {
         IFrameClock frameClock,
         ITerrainTracker terrainTracker,
         IGraphicalDebugger graphicalDebugger,
-        IRegionAnalyzer regionAnalyzer
+        IExpandAnalyzer expandAnalyzer,
+        IRegionAnalyzer regionAnalyzer,
+        ISc2Client sc2Client
     ) {
         _frameClock = frameClock;
         _terrainTracker = terrainTracker;
         _graphicalDebugger = graphicalDebugger;
+        _expandAnalyzer = expandAnalyzer;
         _regionAnalyzer = regionAnalyzer;
+        _sc2Client = sc2Client;
     }
 
-    public Task OnFrame() {
+    public async Task OnFrame() {
         if (_frameClock.CurrentFrame == 0) {
             Logger.Important("Starting map analysis. Expect the game to freeze for a while.");
         }
@@ -38,7 +47,13 @@ public class MapAnalysisBot : IBot {
             // DebugCoordinates();
         }
 
-        return Task.CompletedTask;
+        if (_expandAnalyzer.IsAnalysisComplete && _regionAnalyzer.IsAnalysisComplete && _quitAt == uint.MaxValue) {
+            _quitAt = _frameClock.CurrentFrame + 10; // Just give a few frames to debug the analysis
+        }
+
+        if (_quitAt <= _frameClock.CurrentFrame) {
+            await _sc2Client.LeaveCurrentGame();
+        }
     }
 
     private void DebugCoordinates() {
