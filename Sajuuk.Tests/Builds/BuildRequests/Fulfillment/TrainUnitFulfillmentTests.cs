bdee70901a1b3f301276404941ec1cd05f09bc32
Guillaume Docquier <guillaumedocquier@hotmail.com>
2023-08-23 13:35:50 -0400

    TrainUnitFulfillmentTests

---
+++ Sajuuk.Tests/Builds/BuildRequests/Fulfillment/TrainUnitFulfillmentTests.cs
@@ -0,0 +1,270 @@
+﻿using Moq;
+using Sajuuk.Actions;
+using Sajuuk.Builds.BuildRequests;
+using Sajuuk.Builds.BuildRequests.Fulfillment;
+using Sajuuk.GameData;
+using Sajuuk.GameSense;
+using Sajuuk.MapAnalysis;
+using Sajuuk.Tests.Fixtures;
+
+namespace Sajuuk.Tests.Builds.BuildRequests.Fulfillment;
+
+public class TrainUnitFulfillmentTests : IClassFixture<NoLoggerFixture> {
+    private readonly Mock<IUnitsTracker> _unitsTrackerMock = new Mock<IUnitsTracker>();
+    private readonly Mock<IFrameClock> _frameClockMock = new Mock<IFrameClock>();
+    private readonly KnowledgeBase _knowledgeBase = new TestKnowledgeBase();
+    private readonly Mock<IPathfinder> _pathfinderMock = new Mock<IPathfinder>();
+    private readonly Mock<ITerrainTracker> _terrainTrackerMock = new Mock<ITerrainTracker>();
+    private readonly Mock<IController> _controllerMock = new Mock<IController>();
+    private readonly IActionBuilder _actionBuilder;
+
+    private readonly BuildRequestFulfillmentFactory _buildRequestFulfillmentFactory;
+
+    public TrainUnitFulfillmentTests() {
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns(0);
+        _actionBuilder = new ActionBuilder(_knowledgeBase);
+
+        _buildRequestFulfillmentFactory = new BuildRequestFulfillmentFactory(
+            _unitsTrackerMock.Object,
+            _frameClockMock.Object,
+            _knowledgeBase,
+            _pathfinderMock.Object,
+            new FootprintCalculator(_terrainTrackerMock.Object),
+            _terrainTrackerMock.Object,
+            _controllerMock.Object
+        );
+    }
+
+    private static IEnumerable<object[]> BuildRequestsToSatisfy() {
+        yield return new object[] { new DummyBuildRequest(BuildType.Train, Units.Drone)      , true };
+        yield return new object[] { new DummyBuildRequest(BuildType.Build, Units.Drone)      , false };
+        yield return new object[] { new DummyBuildRequest(BuildType.Research, Units.Drone)   , false };
+        yield return new object[] { new DummyBuildRequest(BuildType.Expand, Units.Drone)     , false };
+        yield return new object[] { new DummyBuildRequest(BuildType.Train, Units.Zergling)   , false };
+        yield return new object[] { new DummyBuildRequest(BuildType.Build, Units.Zergling)   , false };
+        yield return new object[] { new DummyBuildRequest(BuildType.Research, Units.Zergling), false };
+        yield return new object[] { new DummyBuildRequest(BuildType.Expand, Units.Zergling)  , false };
+    }
+
+    [Theory]
+    [MemberData(nameof(BuildRequestsToSatisfy))]
+    public void GivenBuildRequest_WhenCanSatisfy_ThenReturnsTrueIfBuildTypeIsTrainAndUnitTypeMatches(IBuildRequest buildRequest, bool expectedCanSatisfy) {
+        // Arrange
+        var producer = TestUtils.CreateUnit(Units.Larva, _knowledgeBase, actionBuilder: _actionBuilder);
+        const uint unitTypeToTrain = Units.Drone;
+        var producerOrder = producer.TrainUnit(unitTypeToTrain);
+        var trainUnitFulfillment = _buildRequestFulfillmentFactory.CreateTrainUnitFulfillment(producer, producerOrder, unitTypeToTrain);
+
+        // Act
+        var canSatisfy = trainUnitFulfillment.CanSatisfy(buildRequest);
+
+        // Assert
+        Assert.Equal(expectedCanSatisfy, canSatisfy);
+    }
+
+    private static IEnumerable<object[]> ProducerTypes() {
+        yield return new object[] { Units.Larva, Units.Drone };
+        yield return new object[] { Units.Hatchery, Units.Queen };
+    }
+
+    [Theory]
+    [MemberData(nameof(ProducerTypes))]
+    public void GivenProducerIsDeadBeforeExpectedCompletionFrame_WhenUpdateStatus_ThenFulfillmentIsPrevented(uint producerUnitType, uint unitTypeToTrain) {
+        // Arrange
+        var producer = TestUtils.CreateUnit(producerUnitType, _knowledgeBase, actionBuilder: _actionBuilder);
+        var producerOrder = producer.TrainUnit(unitTypeToTrain);
+        var trainUnitFulfillment = _buildRequestFulfillmentFactory.CreateTrainUnitFulfillment(producer, producerOrder, unitTypeToTrain);
+
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns((uint)trainUnitFulfillment.ExpectedCompletionFrame - 1);
+
+        // Act
+        trainUnitFulfillment.UpdateStatus();
+
+        // Assert
+        Assert.Equal(BuildRequestFulfillmentStatus.Prevented, trainUnitFulfillment.Status);
+    }
+
+    [Theory]
+    [MemberData(nameof(ProducerTypes))]
+    public void GivenProducerIsDeadAtExpectedCompletionFrame_WhenUpdateStatus_ThenFulfillmentIsCompleted(uint producerUnitType, uint unitTypeToTrain) {
+        // Arrange
+        var producer = TestUtils.CreateUnit(producerUnitType, _knowledgeBase, actionBuilder: _actionBuilder);
+        var producerOrder = producer.TrainUnit(unitTypeToTrain);
+        var trainUnitFulfillment = _buildRequestFulfillmentFactory.CreateTrainUnitFulfillment(producer, producerOrder, unitTypeToTrain);
+
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns((uint)trainUnitFulfillment.ExpectedCompletionFrame);
+
+        // Act
+        trainUnitFulfillment.UpdateStatus();
+
+        // Assert
+        Assert.Equal(BuildRequestFulfillmentStatus.Completed, trainUnitFulfillment.Status);
+    }
+
+    [Fact]
+    public void GivenProducerIsBuildingAndAliveAtExpectedCompletionFrame_WhenUpdateStatus_ThenFulfillmentIsCompleted() {
+        // Arrange
+        var producer = TestUtils.CreateUnit(Units.Hatchery, _knowledgeBase, actionBuilder: _actionBuilder);
+        const uint unitTypeToTrain = Units.Queen;
+        var producerOrder = producer.TrainUnit(unitTypeToTrain);
+        var trainUnitFulfillment = _buildRequestFulfillmentFactory.CreateTrainUnitFulfillment(producer, producerOrder, unitTypeToTrain);
+
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns((uint)trainUnitFulfillment.ExpectedCompletionFrame);
+        producer.LastSeen = _frameClockMock.Object.CurrentFrame; // Alive
+
+        // Act
+        trainUnitFulfillment.UpdateStatus();
+
+        // Assert
+        Assert.Equal(BuildRequestFulfillmentStatus.Completed, trainUnitFulfillment.Status);
+    }
+
+    private static IEnumerable<object[]> NonMatchingOrders() {
+        yield return new object[] { new List<uint>() };
+        yield return new object[] { new List<uint> { Units.Marine } };
+        yield return new object[] { new List<uint> { Units.Marine, Units.Zealot } };
+    }
+
+    [Theory]
+    [MemberData(nameof(NonMatchingOrders))]
+    public void GivenProducerIsBuildingAndAliveAndHasNoMatchingOrdersAtExpectedCompletionFrame_WhenUpdateStatus_ThenFulfillmentIsCompleted(List<uint> nonMatchingUnitTypes) {
+        // Arrange
+        var producer = TestUtils.CreateUnit(Units.Hatchery, _knowledgeBase, actionBuilder: _actionBuilder);
+        const uint unitTypeToTrain = Units.Queen;
+        var producerOrder = producer.TrainUnit(unitTypeToTrain);
+        var trainUnitFulfillment = _buildRequestFulfillmentFactory.CreateTrainUnitFulfillment(producer, producerOrder, unitTypeToTrain);
+
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns((uint)trainUnitFulfillment.ExpectedCompletionFrame);
+        producer.LastSeen = _frameClockMock.Object.CurrentFrame; // Alive
+
+        producer.Orders.Clear();
+        foreach (var nonMatchingUnitType in nonMatchingUnitTypes) {
+            producer.TrainUnit(nonMatchingUnitType);
+        }
+
+        // Act
+        trainUnitFulfillment.UpdateStatus();
+
+        // Assert
+        Assert.Equal(BuildRequestFulfillmentStatus.Completed, trainUnitFulfillment.Status);
+    }
+
+    [Fact]
+    public void GivenProducerIsUnitAndAliveAtExpectedCompletionFrame_WhenUpdateStatus_ThenFulfillmentStatusIsExecuting() {
+        // Arrange
+        var producer = TestUtils.CreateUnit(Units.Larva, _knowledgeBase, actionBuilder: _actionBuilder);
+        const uint unitTypeToTrain = Units.Drone;
+        var producerOrder = producer.TrainUnit(unitTypeToTrain);
+        var trainUnitFulfillment = _buildRequestFulfillmentFactory.CreateTrainUnitFulfillment(producer, producerOrder, unitTypeToTrain);
+
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns((uint)trainUnitFulfillment.ExpectedCompletionFrame);
+        producer.LastSeen = _frameClockMock.Object.CurrentFrame; // Alive
+
+        // Act
+        trainUnitFulfillment.UpdateStatus();
+
+        // Assert
+        Assert.Equal(BuildRequestFulfillmentStatus.Executing, trainUnitFulfillment.Status);
+    }
+
+    [Theory]
+    [MemberData(nameof(NonMatchingOrders))]
+    public void GivenProducerIsUnitAndAliveAndHasNoMatchingOrdersAtExpectedCompletionFrame_WhenUpdateStatus_ThenFulfillmentIsAborted(List<uint> nonMatchingUnitTypes) {
+        // Arrange
+        var producer = TestUtils.CreateUnit(Units.Larva, _knowledgeBase, actionBuilder: _actionBuilder);
+        const uint unitTypeToTrain = Units.Drone;
+        var producerOrder = producer.TrainUnit(unitTypeToTrain);
+        var trainUnitFulfillment = _buildRequestFulfillmentFactory.CreateTrainUnitFulfillment(producer, producerOrder, unitTypeToTrain);
+
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns((uint)trainUnitFulfillment.ExpectedCompletionFrame);
+        producer.LastSeen = _frameClockMock.Object.CurrentFrame; // Alive
+
+        producer.Orders.Clear();
+        foreach (var nonMatchingUnitType in nonMatchingUnitTypes) {
+            producer.TrainUnit(nonMatchingUnitType);
+        }
+
+        // Act
+        trainUnitFulfillment.UpdateStatus();
+
+        // Assert
+        Assert.Equal(BuildRequestFulfillmentStatus.Aborted, trainUnitFulfillment.Status);
+    }
+
+    private static IEnumerable<object[]> AnyProducerNonMatchingOrders() {
+        yield return new object[] { Units.Larva, Units.Drone, new List<uint>() };
+        yield return new object[] { Units.Larva, Units.Drone, new List<uint> { Units.Marine } };
+        yield return new object[] { Units.Larva, Units.Drone, new List<uint> { Units.Marine, Units.Zealot } };
+
+        yield return new object[] { Units.Hatchery, Units.Queen, new List<uint>() };
+        yield return new object[] { Units.Hatchery, Units.Queen, new List<uint> { Units.Marine } };
+        yield return new object[] { Units.Hatchery, Units.Queen, new List<uint> { Units.Marine, Units.Zealot } };
+    }
+
+    [Theory]
+    [MemberData(nameof(AnyProducerNonMatchingOrders))]
+    public void GivenProducerIsAliveAndHasNoMatchingOrdersBeforeExpectedCompletionFrame_WhenUpdateStatus_ThenFulfillmentIsAborted(uint producerUnitType, uint unitTypeToTrain, List<uint> nonMatchingUnitTypes) {
+        // Arrange
+        var producer = TestUtils.CreateUnit(producerUnitType, _knowledgeBase, actionBuilder: _actionBuilder);
+        var producerOrder = producer.TrainUnit(unitTypeToTrain);
+        var trainUnitFulfillment = _buildRequestFulfillmentFactory.CreateTrainUnitFulfillment(producer, producerOrder, unitTypeToTrain);
+
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns((uint)trainUnitFulfillment.ExpectedCompletionFrame - 1);
+        producer.LastSeen = _frameClockMock.Object.CurrentFrame; // Alive
+
+        producer.Orders.Clear();
+        foreach (var nonMatchingUnitType in nonMatchingUnitTypes) {
+            producer.TrainUnit(nonMatchingUnitType);
+        }
+
+        // Act
+        trainUnitFulfillment.UpdateStatus();
+
+        // Assert
+        Assert.Equal(BuildRequestFulfillmentStatus.Aborted, trainUnitFulfillment.Status);
+    }
+
+    [Theory]
+    [MemberData(nameof(ProducerTypes))]
+    public void GivenFulfillmentIsTerminated_WhenUpdateStatus_ThenStatusDoesNotChange(uint producerUnitType, uint unitTypeToTrain) {
+        // Arrange
+        var producer = TestUtils.CreateUnit(producerUnitType, _knowledgeBase, actionBuilder: _actionBuilder);
+        var producerOrder = producer.TrainUnit(unitTypeToTrain);
+        var trainUnitFulfillment = _buildRequestFulfillmentFactory.CreateTrainUnitFulfillment(producer, producerOrder, unitTypeToTrain);
+
+        var completionFrame = (uint)trainUnitFulfillment.ExpectedCompletionFrame;
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns(completionFrame - 1);
+        producer.LastSeen = _frameClockMock.Object.CurrentFrame - 1; // Died last frame
+
+        trainUnitFulfillment.UpdateStatus(); // Prevented
+
+        _frameClockMock.Setup(frameClock => frameClock.CurrentFrame).Returns(completionFrame);
+        producer.LastSeen = _frameClockMock.Object.CurrentFrame - 1; // Died last frame
+
+        // Act
+        trainUnitFulfillment.UpdateStatus();
+
+        // Assert
+        Assert.Equal(BuildRequestFulfillmentStatus.Prevented, trainUnitFulfillment.Status);
+    }
+
+    private class DummyBuildRequest : IBuildRequest {
+        public DummyBuildRequest(BuildType buildType, uint unitOrUpgradeType) {
+            BuildType = buildType;
+            UnitOrUpgradeType = unitOrUpgradeType;
+        }
+
+        public BuildType BuildType { get; }
+        public uint UnitOrUpgradeType { get; }
+        public int QuantityRequested { get; set; }
+        public uint AtSupply { get; set; }
+        public bool AllowQueueing { get; }
+        public BuildBlockCondition BlockCondition { get; set; }
+        public BuildRequestPriority Priority { get; set; }
+        public int QuantityFulfilled { get; }
+        public int QuantityRemaining { get; }
+        public void AddFulfillment(IBuildRequestFulfillment buildRequestFulfillment) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Sajuuk/Builds/BuildRequests/Fulfillment/TrainUnitFulfillment.cs
+++ Sajuuk/Builds/BuildRequests/Fulfillment/TrainUnitFulfillment.cs
@@ -4,10 +4,12 @@ using SC2APIProtocol;
 
 namespace Sajuuk.Builds.BuildRequests.Fulfillment;
 
-// TODO GD TrainUnitFulfillment does not handle queued orders. Don't queue Train orders!
-// This is not be a huge problem for Zerg because only queens can be queued and since we're a bot we can avoid queuing.
-// i.e Hatchery with 5 queens queued, 1 of them gets canceled or finishes. How do you know which one was yours?
-// UnitOrders do not seem to have an id, so we can't easily reconcile which UnitOrder matches this fulfillment.
+/// <summary>
+/// TrainUnitFulfillment does not handle queued orders. Don't queue Train orders!
+/// This is not be a huge problem for Zerg because only queens can be queued and since we're a bot we can avoid queuing.
+/// i.e Hatchery with 5 queens queued, 1 of them gets canceled or finishes. How do you know which one was yours?
+/// UnitOrders do not seem to have an id, so we can't easily reconcile which UnitOrder matches this fulfillment.
+/// </summary>
 public sealed class TrainUnitFulfillment : BuildRequestFulfillment {
     private readonly IFrameClock _frameClock;
     private readonly KnowledgeBase _knowledgeBase;
@@ -49,8 +51,9 @@ public sealed class TrainUnitFulfillment : BuildRequestFulfillment {
                 Status = BuildRequestFulfillmentStatus.Prevented;
             }
             else {
-                // TODO GD Could the producer get killed exactly on the expected completion frame?
-                // The producer died at or after the expected completion frame, it completed a morph!
+                // The producer died at (or after) the expected completion frame
+                // If the producer was a unit, it completed a morph!
+                // If the producer was a building, I'm going to assume that it both died and produced the unit.
                 Status = BuildRequestFulfillmentStatus.Completed;
             }
         }
@@ -59,12 +62,8 @@ public sealed class TrainUnitFulfillment : BuildRequestFulfillment {
             Status = BuildRequestFulfillmentStatus.Completed;
         }
         else if (_producer.Orders.All(order => order.AbilityId != _producerOrder.AbilityId)) {
-            // Maybe the unit received other orders
             Status = BuildRequestFulfillmentStatus.Aborted;
-        }
-
-        if (Status == BuildRequestFulfillmentStatus.Completed && _frameClock.CurrentFrame != ExpectedCompletionFrame) {
-            Logger.Error($"{this} completed at {_frameClock.CurrentFrame}, which is {_frameClock.CurrentFrame - ExpectedCompletionFrame} frames off target.");
+            Logger.Warning($"\"{this}\" because the producer has no matching order but was not canceled. This is not expected to happen.");
         }
     }
 
