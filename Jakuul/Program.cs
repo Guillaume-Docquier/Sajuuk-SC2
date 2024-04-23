d477bef1ae0b8672d351db7c67e3013c808c3d7f
Guillaume Docquier <guillaumedocquier@hotmail.com>
2024-04-22 22:33:54 -0400

    Added an AiArena CLI parser

--- Jakuul/Program.cs
+++ Jakuul/Program.cs
@@ -2,13 +2,18 @@
 using SC2APIProtocol;
 using SC2Client;
 
-// TODO GD This will be defined by the bot
-var botRace = Race.Zerg;
+var commandLineArgs = new CommandLineArguments(args);
 
 var frameClock = new FrameClock();
 var logger = new Logger(frameClock, logToStdOut: true);
 var sc2Client = new Sc2Client(logger, GameDisplayMode.FullScreen);
-var sc2GameConnection = new LocalGameConnection(logger, sc2Client, new LocalGameConfiguration()); // TODO GD Parse command line arguments
+
+IGameConnection sc2GameConnection = commandLineArgs.LadderServerAddress != null
+    ? new LadderGameConnection(logger, sc2Client, commandLineArgs.LadderServerAddress, commandLineArgs.GamePort, commandLineArgs.StartPort)
+    : new LocalGameConnection(logger, sc2Client, new LocalGameConfiguration());
+
+// TODO GD This will be defined by the bot
+var botRace = Race.Zerg;
 
 // TODO GD Return an IGame instead of player id
 await sc2GameConnection.JoinGame(botRace);
