6fbd4cfb6fbd0229650ad74ccf84aad2b0bb4ea6
Guillaume Docquier <guillaumedocquier@hotmail.com>
2025-01-26 14:37:07 -0500

    Fixed a typo in the named logger where we put log 'message' rather than the actual message

--- SC2Client/NamedLogger.cs
+++ SC2Client/NamedLogger.cs
@@ -18,38 +18,38 @@ public class NamedLogger : ILogger {
     }
 
     public void Debug(string message) {
-        _logger.Debug($"{_prefix} message");
+        _logger.Debug($"{_prefix} {message}");
     }
 
     public void Info(string message) {
-        _logger.Info($"{_prefix} message");
+        _logger.Info($"{_prefix} {message}");
     }
 
     public void Warning(string message) {
-        _logger.Warning($"{_prefix} message");
+        _logger.Warning($"{_prefix} {message}");
     }
 
     public void Error(string message) {
-        _logger.Error($"{_prefix} message");
+        _logger.Error($"{_prefix} {message}");
     }
 
     public void Success(string message) {
-        _logger.Success($"{_prefix} message");
+        _logger.Success($"{_prefix} {message}");
     }
 
     public void Important(string message) {
-        _logger.Important($"{_prefix} message");
+        _logger.Important($"{_prefix} {message}");
     }
 
     public void Performance(string message) {
-        _logger.Performance($"{_prefix} message");
+        _logger.Performance($"{_prefix} {message}");
     }
 
     public void Metric(string message) {
-        _logger.Metric($"{_prefix} message");
+        _logger.Metric($"{_prefix} {message}");
     }
 
     public void Tag(string message) {
-        _logger.Tag($"{_prefix} message");
+        _logger.Tag($"{_prefix} {message}");
     }
 }
