71f3a8616d6c50df722aa388cad6db3833b92a4c
Guillaume Docquier <guillaumedocquier@hotmail.com>
2025-02-01 14:03:12 -0500

    Removed useless code

--- SC2Client/FileLogSink.cs
+++ SC2Client/FileLogSink.cs
@@ -4,12 +4,12 @@ public class FileLogSink : ILogSink {
     private readonly StreamWriter _fileStream;
 
     public FileLogSink(string logFilePath) {
-        // Open a logging file stream
-        // For performance reasons, the file stream is kept open
-        // For safety reasons, the file stream is set to auto flush
-        var logFile = "Logs/" + DateTime.UtcNow.ToString("yyyy-MM-dd HH.mm.ss") + ".log";
         Directory.CreateDirectory(Path.GetDirectoryName(logFilePath)!);
+
+        // For performance reasons, the file stream is kept open
         _fileStream = new StreamWriter(logFilePath, append: true);
+
+        // For safety reasons, the file stream is set to auto flush
         _fileStream.AutoFlush = true;
     }
 
