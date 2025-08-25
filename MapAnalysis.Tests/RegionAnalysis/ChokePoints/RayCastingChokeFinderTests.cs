41b81b57d53a3d86d25391e9f28b5a9ceee7d14d
Guillaume Docquier <guillaumedocquier@hotmail.com>
2025-08-24 21:17:23 -0400

    Extensive vision lines testing (too extensive, but very useful)

--- MapAnalysis.Tests/RegionAnalysis/ChokePoints/RayCastingChokeFinderTests.cs
+++ MapAnalysis.Tests/RegionAnalysis/ChokePoints/RayCastingChokeFinderTests.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using System.Numerics;
 using System.Reflection;
 using MapAnalysis.RegionAnalysis.ChokePoints;
@@ -12,86 +13,77 @@ namespace MapAnalysis.Tests.RegionAnalysis.ChokePoints;
 
 public class RayCastingChokeFinderTests
 {
-    private readonly Mock<ILogger> _loggerMock;
-    private readonly Mock<ITerrainTracker> _terrainTrackerMock;
-    private readonly Mock<IGraphicalDebugger> _graphicalDebuggerMock;
-    private readonly Mock<IMapImageFactory> _mapImageFactoryMock;
-    private readonly Mock<IMapFileNameFormatter> _mapFileNameFormatterMock;
-
-    private readonly RayCastingChokeFinder _chokeFinder;
-
-    private const int MapSize = 20;
-
-    public RayCastingChokeFinderTests()
+    private List<VisionLine> CreateLinesAtAnAngle(IChokeFinder chokeFinder, int mapSize, int angle)
     {
-        _loggerMock = new Mock<ILogger>();
-        _terrainTrackerMock = new Mock<ITerrainTracker>();
-        _graphicalDebuggerMock = new Mock<IGraphicalDebugger>();
-        _mapImageFactoryMock = new Mock<IMapImageFactory>();
-        _mapFileNameFormatterMock = new Mock<IMapFileNameFormatter>();
+        var method = typeof(RayCastingChokeFinder).GetMethod("CreateLinesAtAnAngle", BindingFlags.NonPublic | BindingFlags.Instance);
+        Assert.NotNull(method);
+
+        var result = method.Invoke(chokeFinder, new object[] { angle, mapSize, mapSize });
+        Assert.NotNull(result);
+
+        return (List<VisionLine>)result;
+    }
+
+    private List<VisionLine> BreakDownIntoContinuousSegments(IChokeFinder chokeFinder, List<VisionLine> lines)
+    {
+        var method = typeof(RayCastingChokeFinder).GetMethod("BreakDownIntoContinuousSegments", BindingFlags.NonPublic | BindingFlags.Instance, null, new[] { typeof(IEnumerable<VisionLine>) }, null);
+        Assert.NotNull(method);
+
+        var result = method.Invoke(chokeFinder, new object[] { lines });
+        Assert.NotNull(result);
+
+        return (List<VisionLine>)result;
+    }
+
+    public static IEnumerable<object[]> LinesAtAnAngleData() {
+        for (int angle = 0; angle < 180; angle++) {
+            for (int mapSize = 0; mapSize < 250; mapSize += 15) {
+                yield return new object[] { angle, mapSize };
+            }
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(LinesAtAnAngleData))]
+    public void CreateLinesAtAnAngle_WithAngle_ShouldCoverAllCells(int angle, int mapSize)
+    {
+        // Arrange
+        var terrainTrackerMock = new Mock<ITerrainTracker>();
 
         var walkableCells = new HashSet<Vector2>();
-        for (var x = 0; x < MapSize; x++)
+        for (var x = 0; x < mapSize; x++)
         {
-            for (var y = 0; y < MapSize; y++)
+            for (var y = 0; y < mapSize; y++)
             {
                 walkableCells.Add(new Vector2(x, y));
             }
         }
 
-        _terrainTrackerMock.Setup(t => t.Cells).Returns(walkableCells);
-        _terrainTrackerMock.Setup(t => t.MaxX).Returns(MapSize);
-        _terrainTrackerMock.Setup(t => t.MaxY).Returns(MapSize);
+        terrainTrackerMock.Setup(t => t.Cells).Returns(walkableCells);
+        terrainTrackerMock.Setup(t => t.MaxX).Returns(mapSize);
+        terrainTrackerMock.Setup(t => t.MaxY).Returns(mapSize);
 
-        _chokeFinder = new RayCastingChokeFinder(
-            _loggerMock.Object,
-            _terrainTrackerMock.Object,
-            _graphicalDebuggerMock.Object,
-            _mapImageFactoryMock.Object,
-            _mapFileNameFormatterMock.Object,
+        var chokeFinder = new RayCastingChokeFinder(
+            new NoLogger(),
+            terrainTrackerMock.Object,
+            new NullGraphicalDebugger(),
+            new NoMapImageFactory(),
+            new MapFileNameFormatter("don't care"),
             "test_map"
         );
-    }
 
-    private List<VisionLine> CreateLinesAtAnAngle(int angle)
-    {
-        var method = typeof(RayCastingChokeFinder).GetMethod("CreateLinesAtAnAngle", BindingFlags.NonPublic | BindingFlags.Instance);
-        Assert.NotNull(method);
-
-        var result = method.Invoke(_chokeFinder, new object[] { angle, MapSize, MapSize });
-        Assert.NotNull(result);
-
-        return (List<VisionLine>)result;
-    }
-
-    private List<VisionLine> BreakDownIntoContinuousSegments(List<VisionLine> lines)
-    {
-        var method = typeof(RayCastingChokeFinder).GetMethod("BreakDownIntoContinuousSegments", BindingFlags.NonPublic | BindingFlags.Instance, null, new[] { typeof(IEnumerable<VisionLine>) }, null);
-        Assert.NotNull(method);
-
-        var result = method.Invoke(_chokeFinder, new object[] { lines });
-        Assert.NotNull(result);
-
-        return (List<VisionLine>)result;
-    }
-
-    [Theory]
-    [InlineData(0)]
-    [InlineData(45)]
-    [InlineData(79)]
-    [InlineData(90)]
-    [InlineData(135)]
-    public void CreateLinesAtAnAngle_WithAngle_ShouldCoverAllCells(int angle)
-    {
-        // Arrange
-        var walkableCellsCount = _terrainTrackerMock.Object.Cells.Count;
+        var walkableCellsCount = terrainTrackerMock.Object.Cells.Count;
 
         // Act
-        var rawLines = CreateLinesAtAnAngle(angle);
-        var lines = BreakDownIntoContinuousSegments(rawLines);
-        var coveredCellsCount = lines.SelectMany(line => line.OrderedTraversedCells).ToHashSet().Count;
+        var rawLines = CreateLinesAtAnAngle(chokeFinder, mapSize, angle);
+        var lines = BreakDownIntoContinuousSegments(chokeFinder, rawLines);
+        var coveredCells = lines.SelectMany(line => line.OrderedTraversedCells).ToHashSet();
+
+        // var mapImage = new MapImage(new NoLogger(), mapSize, mapSize);
+        // mapImage.SetCellsColor(coveredCells, Color.Green);
+        // mapImage.Save($"./CreateLinesAtAnAngle_WithAngle_ShouldCoverAllCells_{angle}");
 
         // Assert
-        Assert.Equal(walkableCellsCount, coveredCellsCount);
+        Assert.Equal(walkableCellsCount, coveredCells.Count);
     }
 }
